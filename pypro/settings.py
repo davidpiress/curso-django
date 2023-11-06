27f933af0a4d3380ddfa90a984d9d760694bd905
Davi <davi4667@gmail.com>
2023-11-05 21:25:51 -0300

    foi criado as configurações iniciais

--- pypro/settings.py
+++ pypro/settings.py
@@ -89,14 +89,14 @@ parser_database = partial(dj_database_url.parse, conn_max_age=600)
 DATABASES = {
     'default': config('DATABASE_URL', default=default_db_url, cast=parser_database)
 }
-
+"""
 DATA_BASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
